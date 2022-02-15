d4a3b7c57d5c0e45f6a20b44adc8922437040124
Prachi1808 <prachimohanty.1808@gmail.com>
2022-02-15 21:30:43 +0530

    Snake and ladder game

---
+++ src/main/SnakeAndLadders.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Scanner;
+
+public class SnakeAndLadders {
+	public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Input the names of the one players:");
+        String A=sc.next();      
+        int playerAposition=0;
+        int count=0;
+        int x;
+}
+
+}	
